f982ee0c7061b58f6062ecadf880aea91a1e0721
asre459 <asre45900@gmail.com>
2025-08-18 05:59:22 -0300

    add the frontend url to the backend

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,17 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173'];
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+     origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
